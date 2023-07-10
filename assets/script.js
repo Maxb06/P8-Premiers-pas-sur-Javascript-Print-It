4a0a6d618e329c9d03a877eb850ba575e60ae7f7
Maxime Brunet <maxime.brunet@orange.fr>
2023-07-10 14:44:24 +0200

    màj finale code javascript

--- assets/script.js
+++ assets/script.js
@@ -22,14 +22,6 @@ const slides = [
 const arrowLeft = document.querySelector('.arrow_left');  // Récupération des éléments flèches G/D
 const arrowRight = document.querySelector('.arrow_right');
 
-arrowLeft.addEventListener('click', function() {  // Ajout des Event Listeners G/D
-  console.log("Clic flèche gauche");
-});
-
-arrowRight.addEventListener('click', function() {
-  console.log("Clic flèche droite");
-});
-
 /* Étape 3 : Ajoutez des bullet points au slider */
 
 const numberOfSlides = slides.length;
@@ -46,12 +38,12 @@ if (firstDot) {
   firstDot.classList.add('dot_selected');
 }
 
-/* Etape 4 */
+/* Etapes 4 - 5 */
 
 let currentSlide = 0; // Variable pour suivre l'index de la diapositive actuelle
 
 arrowLeft.addEventListener('click', function() {
-  
+  console.log("Clic flèche gauche");
   currentSlide--;  // Décrémenter l'index de la diapositive actuelle
 
   if (currentSlide < 0) {  // Vérifier si index est inférieur à zéro pour revenir à la dernière diapositive
@@ -74,10 +66,10 @@ arrowLeft.addEventListener('click', function() {
 });
 
 arrowRight.addEventListener('click', function() {
-  
+  console.log("Clic flèche droite");
   currentSlide++;  // Incrémenter index de la diapo actuelle
 
-  if (currentSlide >= numberOfSlides) { // check si index est sup ou = au nombre de diapos pour revenir à la première diapositive
+  if (currentSlide >= numberOfSlides) { // check si index est sup ou = au nombre de diapos pour revenir à la 1ere diapositive
     currentSlide = 0;
   }
 
@@ -95,28 +87,3 @@ arrowRight.addEventListener('click', function() {
     }
   });
 });
-
-/* Etape 5 */
-
-setInterval(function() {  // Défilement infini sur le carrousel
-	
-	currentSlide++;  // Incrémenter l'index de la diapositive actuelle
-  
-	if (currentSlide >= numberOfSlides) {  // check si index est sup ou = au nombre total de diapo pour revenir à la 1ere diapo
-	  currentSlide = 0;
-	}
-  
-	const slide = slides[currentSlide];          // Mise à jour image et texte correspondants à la diapositive actuelle
-	const image = document.querySelector('.banner-img');
-	const tagLine = document.querySelector('#banner p');
-	image.src = './assets/images/slideshow/' + slide.image;
-	tagLine.innerHTML = slide.tagLine;
-  
-	const dots = dotsContainer.querySelectorAll('.dot');   // Mettre à jour le bullet point actif
-	dots.forEach((dot, index) => {
-	  dot.classList.remove('dot_selected');
-	  if (index === currentSlide) {
-		dot.classList.add('dot_selected');
-	  }
-	});
-  }, 5000); // Défilement toutes les 5 secondes   
